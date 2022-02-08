219caa5470e1dbe69242d330f8836edfea6ce93f
mohraf <amosngok@gmail.com>
2022-02-08 09:00:55 +0300

    add registration form

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,7 +1,9 @@
+import email
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import Required, Email, Length, EqualTo
 from wtforms import ValidationError
+from ..models import User
 
 
 class LoginForm(FlaskForm):
@@ -9,3 +11,21 @@ class LoginForm(FlaskForm):
   password = PasswordField('Password', validators=[Required()])
   remember = BooleanField('Remember me')
   submit = SubmitField('Sign In')
+
+
+class RegistrationForm(FlaskForm):
+  email = StringField('Your Email Address', validators=[Required(), Email()])
+  username = StringField('Enter your username', validators=[Required()])
+  password = PasswordField('Password', validators=[Required(),
+  EqualTo('password2', message='Passwords must match')])
+  password2 = PasswordField('Confirm Password', validators=[Required()])
+  submit = SubmitField('Sign Up')
+
+  def validate_email(self, data_field):
+    if User.query.filter_by(email=data_field.data).first():
+      raise ValidationError('There is an account with that Email')
+  
+  def validate_username(self, data_field):
+    if User.query.filter_by(username=data_field.data).first():
+      raise ValidationError('That username is taken')
+  
