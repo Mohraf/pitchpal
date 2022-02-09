d797096f8b62b2c42ec666893a78c62972e6dcbd
mohraf <amosngok@gmail.com>
2022-02-09 12:57:02 +0300

    add profile view and template

--- app/main/views.py
+++ app/main/views.py
@@ -2,6 +2,7 @@ from flask import render_template, request, redirect, url_for, abort
 from . import main
 from flask_login import current_user, login_required
 from .. import db
+from ..models import User
 
 import markdown2
 
@@ -13,5 +14,16 @@ def index():
   '''
 
   title = 'Home - Welcome to PitchPal'
+  user = current_user.username
 
-  return render_template('index.html', title=title)
+  return render_template('index.html', title=title, user=user)
+
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
